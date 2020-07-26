5a586d628743ff748d724d53953d838cafc4868d
Sanket Shah <mesanketshah@gmail.com>
2020-07-26 22:14:50 +0530

    Issues

--- application/views/credit_sale/list.php
+++ application/views/credit_sale/list.php
@@ -17,12 +17,12 @@ $this->load->view('common/left_panel');
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <?= form_open('Sales_Summary/search', ['id' => 'serch_date']); ?>
+                <?= form_open('Credit_Sales_Report/search', ['id' => 'serch_date']); ?>
                 <div class="form-group row" style="padding-top: 20px;">
                     <label class="col-md-2"> select Date<span style="color: red">*</span> <span id="purchase_date_error" style="color: red"></span></label>
                     <div class="col-md-3">
                         <!--<input type="text" name="purchase_date" id="purchase_date" class="form-control datepicker" placeholder="Purchase Date" required>-->
-                        <input type="text" class="form-control" name="daterange" value="" />
+                        <input type="text" class="form-control" autocomplete="off" name="daterange" value="" />
                     </div>
                     <div class="col-md-2">
                         <button type="submit" class="btn btn-success">Search</button>
